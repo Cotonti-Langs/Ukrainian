4c2f882d7afeacb2e76e5e293c45ad4d6f83579d
Pavel Tkachenko <dayver@dayver.net>
2012-01-25 13:34:46 +0200

    Update plugins/whosonline/lang/whosonline.ua.lang.php

--- plugins/whosonline/lang/whosonline.ua.lang.php
+++ plugins/whosonline/lang/whosonline.ua.lang.php
@@ -5,13 +5,13 @@
  * @package whosonline
  * @version 0.9.5
  * @author Cotonti Translators Team
- * @copyright Copyright (c) Cotonti Team 2011
+ * @copyright Copyright (c) Cotonti Team 2008-2012
  * @license BSD
  */
 
 defined('COT_CODE') or die('Wrong URL.');
 
-$L['info_desc'] = 'Відображає кількість користувачів онлайн на головній та інших сторінках';
+$L['info_desc'] = 'Відображає кількість користувачів онлайн на головній та окремій сторінці';
 
 /**
  * Plugin Title & Subtitle
@@ -27,7 +27,7 @@ $L['MostOnline'] = 'Найбільша кількість відвідувачі
 $L['NowOnline'] = 'Зараз на сайті';
 $L['LastSeen'] = 'Дата і час';
 $L['Location'] = 'Розділ сайту';
-$L['IPaddress'] = 'IP address';
+$L['IPaddress'] = 'IP адреса';
 $L['Visitor'] = 'Гість';
 
 ?>
