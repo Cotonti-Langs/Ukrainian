69f304777d57ebb75366793907779ea487dec2de
Dayver <dayver@dayver.net>
2015-01-20 04:49:34 +0200

    Another fix for #8 - Second step
    All core, themes and user module and tags plugin lang files done.
    In third step remains to fix other modules and plugins lang files

--- modules/users/lang/users.ua.lang.php
+++ modules/users/lang/users.ua.lang.php
@@ -2,23 +2,32 @@
 /**
  * Ukrainian Language File for the Users Module
  *
- * @package users
- * @version 0.9.5
- * @author Cotonti Translators Team
- * @copyright Copyright (c) Cotonti Team 2011
- * @license BSD
+ * @package Users
+ * @copyright (c) Cotonti Translators Team
+ * @license https://github.com/Cotonti/Cotonti/blob/master/License.txt
  */
 
 defined('COT_CODE') or die('Wrong URL.');
 
-$L['info_desc'] = 'Включає реєстрацію, профілі та списки користувачів';
+$L['info_desc'] = 'Реєстрація та керування користувачами, профілі і сторінки користувачів';
 
-$L['cfg_disablereg'] = array('Вимкнути реєстрацію ',' Заборонити реєстрацію нових користувачів');
-$L['cfg_maxusersperpage'] = array('Максимальна кількість записів на сторінку в списку користувачів', ' ');
-$L['cfg_regnoactivation'] = array('Скасувати перевірку e-mail при реєстрації ',' З причини безпеки рекомендується &laquo;Ні&raquo;!');
-$L['cfg_regrequireadmin'] = array('Затвердження нових облікових записів адміністратором', ' ');
-$L['cfg_user_email_noprotection'] = array('Вимкнути захист зміни e-mail з паролем', 'З причини безпеки рекомендується &laquo;Ні&raquo;!');
-$L['cfg_useremailchange'] = array('Дозволити користувачам змінювати свій e-mail', 'З причини безпеки рекомендується &laquo;Ні&raquo;!');
-$L['cfg_usertextmax'] = array('Максимальна довжина підпису, символів', '');
+$L['cfg_disablereg'] = 'Відключити реєстрацію';
+$L['cfg_disablereg_hint'] = 'Заборонити реєстрацію нових користувачів';
+$L['cfg_maxusersperpage'] = 'Максимальна кількість записів на сторінку в списку користувачів';
+$L['cfg_maxusersperpage_hint'] = '';
+$L['cfg_regnoactivation'] = 'Скасувати перевірку e-mail при реєстрації';
+$L['cfg_regnoactivation_hint'] = 'З причини безпеки рекомендується &laquo;Ні&raquo;!';
+$L['cfg_regrequireadmin'] = 'Затвердження нових облікових записів адміністратором';
+$L['cfg_regrequireadmin_hint'] = '';
+$L['cfg_user_email_noprotection'] = 'Вимкнути захист зміни e-mail з паролем';
+$L['cfg_user_email_noprotection_hint'] = 'З причини безпеки рекомендується &laquo;Ні&raquo;!';
+$L['cfg_useremailchange'] = 'Дозволити користувачам змінювати свій e-mail';
+$L['cfg_useremailchange_hint'] = 'З причини безпеки рекомендується &laquo;Ні&raquo;!';
+$L['cfg_usertextmax'] = 'Максимальна довжина підпису, символів';
+$L['cfg_usertextmax_hint'] = '';
+$L['cfg_usertextimg'] = 'Дозволити зображення і HTML-код в підписах користувачів';
+$L['cfg_usertextimg_hint'] = 'З причини безпеки рекомендується &laquo;Ні&raquo;!';
 
-?>
+$L['Password_updated'] = 'Пароль змінений';
+$L['Profile_updated'] = 'Налаштування профілю збережені';
+$L['User_data_updated'] = 'Дані користувача збережені';
